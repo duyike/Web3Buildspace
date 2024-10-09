9c5ca84e7e9168d1013aec81cb2c5551bd0e9615
yikedu <duyike2021@gmail.com>
2024-10-09 23:15:58 +0800

    feat: sol 101

--- sol101/send_txn.js
+++ sol101/send_txn.js
@@ -1,29 +1,30 @@
 import solana from "@solana/web3.js";
-
+import { connection } from "./connection.js";
 async function sendTxn() {
-    const fromPrivateKey = process.env.PRIVATE_KEY_A;
-    const fromKeypair = solana.Keypair.fromSecretKey(
-      Uint8Array.from(Buffer.from(fromPrivateKey, "hex"))
+  const fromPrivateKey = process.env.PRIVATE_KEY_A;
+  const fromKeypair = solana.Keypair.fromSecretKey(
+    Uint8Array.from(Buffer.from(fromPrivateKey, "hex"))
+  );
+  const toPublicKey = new solana.PublicKey(process.env.PUBLIC_KEY_B);
+
+  let transaction = new solana.Transaction().add(
+    solana.SystemProgram.transfer({
+      fromPubkey: fromKeypair.publicKey,
+      toPubkey: toPublicKey,
+      lamports: 0.1 * solana.LAMPORTS_PER_SOL,
+    })
+  );
+
+  try {
+    const txnSignature = await solana.sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [fromKeypair]
     );
-    const toPublicKey = new solana.PublicKey(process.env.PUBLIC_KEY_B);
-
-    const connection = new solana.Connection(solana.clusterApiUrl("devnet"), "confirmed");
-
-    let transaction = new solana.Transaction().add(
-        solana.SystemProgram.transfer({
-            fromPubkey: fromKeypair.publicKey,
-            toPubkey: toPublicKey,
-            lamports: 0.1 * solana.LAMPORTS_PER_SOL,
-        })
-    )
-
-    try {
-        const txnSignature = await solana.sendAndConfirmTransaction(connection, transaction, [fromKeypair]);
-        console.log(txnSignature);
-    } catch (error) {
-        console.error(error);
-    }
-
+    console.log(txnSignature);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 sendTxn();
